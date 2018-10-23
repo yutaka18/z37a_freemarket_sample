5e0d01f3e653716a3d71af561c4dab36d4a1e0c4
yutaka18 <ibayutaka@aibayutakanoMacBook-puro.local>
2018-10-23 17:35:33 +0900

    Add (items_html)

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  root    'tests#index'
-  resources :tests
+  root    'items#index'
+  resources :items
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
