2d0de1ecca393d1f9bea5213f4ae732c5d234ac6
yutaka18 <ibayutaka@aibayutakanoMacBook-puro.local>
2018-10-23 17:24:46 +0900

    Add (test_html)

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
+  root    'tests#index'
+  resources :tests
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
