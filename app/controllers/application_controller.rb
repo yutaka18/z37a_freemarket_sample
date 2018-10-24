21b96a61684f7bb8b80ede439f53c04134c5acc9
yutaka18 <ibayutaka@aibayutakanoMacBook-puro.local>
2018-10-24 17:19:09 +0900

    edit basic

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,5 @@
 class ApplicationController < ActionController::Base
-  # before_action :basic_auth, if: :production?
+  before_action :basic_auth, if: :production?
   protect_from_forgery with: :exception
 
   private
@@ -8,9 +8,15 @@ class ApplicationController < ActionController::Base
     Rails.env.production?
   end
 
+  # def basic_auth
+  #   authenticate_or_request_with_http_basic do |username, password|
+  #     username == ENV["BASIC_AUTH_USER"] && password == ENV["BASIC_AUTH_PASSWORD"]
+  #   end
+  # end
+
   def basic_auth
     authenticate_or_request_with_http_basic do |username, password|
-      username == ENV["BASIC_AUTH_USER"] && password == ENV["BASIC_AUTH_PASSWORD"]
+      username == 'admin' && password == '0000'
     end
   end
 
