9f17ac80095f569a59cc58b4a47f1a5fcb0ba1bb
yutaka18 <ibayutaka@aibayutakanoMacBook-puro.local>
2018-10-24 12:30:52 +0900

    Add(basic_production)

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,12 @@
 class ApplicationController < ActionController::Base
+  before_action :basic_auth, if: :production?
   protect_from_forgery with: :exception
-  before_action :basic_auth
 
   private
+
+  def production?
+    Rails.env.production?
+  end
 
   def basic_auth
     authenticate_or_request_with_http_basic do |username, password|
