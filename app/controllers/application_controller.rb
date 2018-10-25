dbc661fea1670265a7950c482fcea088e7569ff6
yutaka18 <ibayutaka@aibayutakanoMacBook-puro.local>
2018-10-25 19:02:43 +0900

    add(basic_test7)

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,5 @@
 class ApplicationController < ActionController::Base
-  before_action :basic_auth, if: :production?, :authenticate_user!
+  # before_action :basic_auth, if: :production?, :authenticate_user!
   protect_from_forgery with: :exception
   before_action :configure_permitted_parameters, if: :devise_controller?
   
@@ -9,22 +9,24 @@ class ApplicationController < ActionController::Base
     Rails.env.production?
   end
 
-  # def basic_auth
-  #   authenticate_or_request_with_http_basic do |username, password|
-  #     username == ENV["BASIC_AUTH_USER"] && password == ENV["BASIC_AUTH_PASSWORD"]
-  #   end
-  # end
-
   def basic_auth
     authenticate_or_request_with_http_basic do |username, password|
-      username == 'admin' && password == '0000'
+      username == ENV["BASIC_AUTH_USER"] && password == ENV["BASIC_AUTH_PASSWORD"]
+      logger.debug(ENV["BASIC_AUTH_USER"])
+      logger.debug(ENV["BASIC_AUTH_PASSWORD"])
     end
   end
 
-  protected
+  # def basic_auth
+  #   authenticate_or_request_with_http_basic do |username, password|
+  #     username == 'admin' && password == '0000'
+  #   end
+  # end
 
-  def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :phone_number, :post_number, :prefecture, :city, :address, :last_name, :first_name, :last_name_kana, :first_name_kana, :birth_year, :birth_month, :birth_day])
-  end
+  # protected
+
+  # def configure_permitted_parameters
+  #   devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :phone_number, :post_number, :prefecture, :city, :address, :last_name, :first_name, :last_name_kana, :first_name_kana, :birth_year, :birth_month, :birth_day])
+  # end
 
 end
