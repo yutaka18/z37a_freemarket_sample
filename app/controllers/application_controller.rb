ab59b05f5bc3db5ca85a8bc8e2dac26fb34a7405
yutaka18 <ibayutaka@aibayutakanoMacBook-puro.local>
2018-10-25 17:53:51 +0900

    add(basic_test7)

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,16 +8,16 @@ class ApplicationController < ActionController::Base
     Rails.env.production?
   end
 
-  def basic_auth
-    authenticate_or_request_with_http_basic do |username, password|
-      username == ENV["BASIC_AUTH_USER"] && password == ENV["BASIC_AUTH_PASSWORD"]
-    end
-  end
-
   # def basic_auth
   #   authenticate_or_request_with_http_basic do |username, password|
-  #     username == 'test' && password == '1111'
+  #     username == ENV["BASIC_AUTH_USER"] && password == ENV["BASIC_AUTH_PASSWORD"]
   #   end
   # end
+
+  def basic_auth
+    authenticate_or_request_with_http_basic do |username, password|
+      username == 'admin' && password == 'unagi'
+    end
+  end
 
 end
