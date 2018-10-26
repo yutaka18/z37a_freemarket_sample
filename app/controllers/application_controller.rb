75d7ae766abf77e68c13f867c98a32a53ef85032
yutaka18 <ibayutaka@aibayutakanoMacBook-puro.local>
2018-10-26 14:11:33 +0900

    fix

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,7 +11,6 @@ class ApplicationController < ActionController::Base
 
   def basic_auth
     logger(ENV["BASIC_AUTH_USER"])
-    logger(ENV["BASIC_AUTH_PASSWORD"])
     authenticate_or_request_with_http_basic do |username, password|
       username == ENV["BASIC_AUTH_USER"] && password == ENV["BASIC_AUTH_PASSWORD"]
     end
