bd9dd6645b573b51ab724de1635209cb3097555f
yutaka18 <youf18@gmail.com>
2018-11-06 21:07:36 +0900

    Add (user_create&destination_create)

--- app/models/destination.rb
+++ app/models/destination.rb
@@ -1,11 +1,11 @@
 class Destination < ApplicationRecord
-	belongs_to :user
+  belongs_to :user
 
-    validates :first_name, presence: true
-    validates :last_name, presence: true
-    validates :first_name_kana, presence: true, format: { with: /\A^[ア-ン゛゜ァ-ォャ-ョー「」、]+$\z/ , message: "は全角カタカナで入力してください" }
-    validates :last_name_kana, presence: true, format: { with: /\A^[ア-ン゛゜ァ-ォャ-ョー「」、]+$\z/ , message:  "は全角カタカナで入力してください" }
-    validates :post_number, presence: true, format: { with: /\A^\d{7}$\z/, message:  "は半角数字のみで7桁入力してください" }
-    validates :city, presence: true, format: { with: /\A^\d{7}$\z/ }
-    validates :address, presence: true
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :first_name_kana, presence: true, format: { with: /\A^[ア-ン゛゜ァ-ォャ-ョー「」、]+$\z/ , message: "は全角カタカナで入力してください" }
+  validates :last_name_kana, presence: true, format: { with: /\A^[ア-ン゛゜ァ-ォャ-ョー「」、]+$\z/ , message:  "は全角カタカナで入力してください" }
+  validates :post_number, presence: true, format: { with: /\A^\d{7}$\z/, message:  "は半角数字のみで7桁入力してください" }
+  validates :city, presence: true, format: { with: /\A^\d{7}$\z/ }
+  validates :address, presence: true
 end
