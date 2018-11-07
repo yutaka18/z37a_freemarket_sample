cd35e58d0fb9bd997b32990b1587e3bac80b50c3
yutaka18 <youf18@gmail.com>
2018-11-07 10:41:27 +0900

    Add (user_login)

--- app/models/destination.rb
+++ app/models/destination.rb
@@ -6,6 +6,6 @@ class Destination < ApplicationRecord
   validates :first_name_kana, presence: true, format: { with: /\A^[ア-ン゛゜ァ-ォャ-ョー「」、]+$\z/ , message: "は全角カタカナで入力してください" }
   validates :last_name_kana, presence: true, format: { with: /\A^[ア-ン゛゜ァ-ォャ-ョー「」、]+$\z/ , message:  "は全角カタカナで入力してください" }
   validates :post_number, presence: true, format: { with: /\A^\d{7}$\z/, message:  "は半角数字のみで7桁入力してください" }
-  validates :city, presence: true, format: { with: /\A^\d{7}$\z/ }
+  validates :city, presence: true
   validates :address, presence: true
 end
