fcbc998efbbea46e4b92d67654601781610ff877
d_riko1130 <free.and.freedom1130@gmail.com>
2018-11-08 21:49:00 +0900

    Add test to spec item_content,price

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -18,5 +18,57 @@ describe Item do
       item = build(:item, name: 'a'*40 )
       expect(item).to be_valid
     end
+
+    it "is invalid without a content" do
+      item = build(:item, content: nil)
+      item.valid?
+      expect(item.errors[:content]).to include("を入力してください")
+    end
+
+    it "is invalid with a content that has more than 1001 characters" do
+      item = build(:item, content: 'a'*1001)
+      item.valid?
+      expect(item.errors[:content]).to include("は1000文字以内で入力してください")
+    end
+
+    it "is valid with a content that has less than 1000 characters" do
+      item = build(:item, content: 'a'*1000)
+      expect(item).to be_valid
+    end
+
+    it "is invalid without a price" do
+      item = build(:item, price: nil)
+      item.valid?
+      expect(item.errors[:price]).to include("を入力してください")
+    end
+
+    it "is invalid with a price that has varchar" do
+      item = build(:item, price: '')
+      item.valid?
+      expect(item.errors[:price]).to include("は数値で入力してください")
+    end
+
+    it "is invalid with a price that has less than 299 integer" do
+      item = build(:item, price: 299 )
+      item.valid?
+      expect(item.errors[:price]).to include("は300以上の値にしてください")
+    end
+
+    it "is valid with a price that has more than 300 integer" do
+      item = build(:item, price: 300 )
+      expect(item).to be_valid
+    end
+
+    it "is valid with a price that has less than 9999999 integer" do
+      item = build(:item, price: 9999999 )
+      expect(item).to be_valid
+    end
+
+    it "is invalid with a price that has more than 10000000 integer" do
+      item = build(:item, price: 10000000 )
+      item.valid?
+      expect(item.errors[:price]).to include("は9999999以下の値にしてください")
+    end
+
   end
 end
