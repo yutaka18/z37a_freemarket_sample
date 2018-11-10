8f7440ea1952e963aa11769ca95e962ecbbb0f9f
yutaka18 <youf18@gmail.com>
2018-11-10 20:04:46 +0900

    Add (category_import)

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -14,7 +14,7 @@ CarrierWave.configure do |config|
       aws_secret_access_key: Rails.application.secrets.aws_secret_access_key,
       region: 'ap-northeast-1'
     }
-
+  end
   config.fog_directory  = 'yutaka-update'
   config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/yutaka-update'
 end
