7afc19dec1e8fdc9298c9faea52227851cb99b02
yamazaki27 <42567345+yamazaki27@users.noreply.github.com>
2018-11-13 18:33:45 +0900

    Delete items_show.js

--- app/assets/javascripts/items_show.js
+++
@@ -1,7 +0,0 @@
-$(function(){
-  $('.owl-dots li').mouseover(function(e) {
-    $('.owl-item').removeClass("active");
-    $('.owl-dot').removeClass("active");
-    $('this').addClass("active")
-  });
-});
